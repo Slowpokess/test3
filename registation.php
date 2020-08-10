893ce8c3c80ff16351bbe198e08086d94515e2b8
Slowpokess <68640835+Slowpokess@users.noreply.github.com>
2020-08-10 05:53:52 +0300

    Update registation.php

--- registation.php
+++ registation.php
@@ -1,18 +1,36 @@
 <?php
 // define variables and set to empty values
-$name = $email = $website = "";
+$nameErr = $emailErr = "";
+$name = $email = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $website = test_input($_POST["website"]);
-
-}
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+ 
 
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
-  return $data;
+  return $data; 
+}
 }
 ?>
